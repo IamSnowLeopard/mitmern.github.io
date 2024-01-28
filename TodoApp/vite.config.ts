83dbceacfa09aa9ba7c45e70ccaf81ef0216808d
IamSnowLeopard <146442423+IamSnowLeopard@users.noreply.github.com>
2024-01-28 17:53:54 -0500

    bug fixes

--- TodoApp/vite.config.ts
+++ TodoApp/vite.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: 'iamsnowleopard.github.io/mitmern.github.io/TodoApp'
+  base: '/IamSnowLeopard.github.io/TodoApp'
 })
